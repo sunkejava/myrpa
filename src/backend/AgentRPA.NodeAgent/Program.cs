47d2c6e93507c42287f46f853842458a84afd24a
declineaberdeen <declineaberdeen@foxmail.com>
2026-09-16 00:15:49 +0800

    feat(node-agent): register workflow runtime

--- src/backend/AgentRPA.NodeAgent/Program.cs
+++ src/backend/AgentRPA.NodeAgent/Program.cs
@@ -1,9 +1,9 @@
 using AgentRPA.NodeAgent;
+using AgentRPA.NodeAgent.Execution;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
-
 var serverUrl = builder.Configuration["NodeAgent:ServerUrl"] ?? "https://localhost:5001";
 builder.Services.Configure<NodeAgentOptions>(builder.Configuration.GetSection("NodeAgent"));
 builder.Services.AddHttpClient("AgentRPA.Server", client =>
@@ -11,6 +11,6 @@ builder.Services.AddHttpClient("AgentRPA.Server", client =>
     client.BaseAddress = new Uri(serverUrl.TrimEnd('/') + "/");
     client.Timeout = TimeSpan.FromSeconds(30);
 });
+builder.Services.AddSingleton<IWorkflowRuntime, PlaywrightWorkflowRuntime>();
 builder.Services.AddHostedService<NodeAgentWorker>();
-
 await builder.Build().RunAsync();
